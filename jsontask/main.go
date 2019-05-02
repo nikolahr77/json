9bd2668bd8c36bc6e1a6bf76b12eb0bf7fd01963
Nikola Hristov <nikolahr0707@icloud.com>
2019-05-02 15:28:18 +0300

    marshaled struct and printed to console

--- jsontask/main.go
+++ jsontask/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	FirstName  string
+	LastName   string
+	Age        int
+	Address    string
+	IsMarriege bool
+}
+
+func main() {
+	p := Person{"Ivan", "Petrov", 25, "Sofia, Triadista 155", false}
+
+	b, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(b)
+
+}
